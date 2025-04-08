d7a7c93d7aa250c3f4be28d196ca9347ddae6ab4
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-04-08 14:58:00 +0200

    update

---
+++ client/src/styles/GlobalStyles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+import './normalize.css';
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+    }
+
+    img {
+        max-width: 100%;
+        display: block;
+    }
+
+    ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    body{
+        margin: 0;
+        
+        &.overflow{
+            overflow: hidden;
+        }
+    }
+
+    #root{
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    h1, h2, h3, h4, h5, h6, p {
+        margin: 0;
+    } 
+
+`;
---
+++ client/src/styles/fonts.ts
@@ -0,0 +1,19 @@
+export const FONT_FAMILY = {
+	chewy: '"Chewy", serif;',
+	pacifico: '"Pacifico", serif;',
+	grandstander: '"Grandstander", serif;'
+};
+
+export const FONT_WEIGHT = {
+	regular: '400',
+	bold: '700'
+};
+
+export const FONT_SIZE = {
+	header: '3rem',
+	xl: '2.25rem',
+	l: '2rem',
+	m: '1.5rem',
+	s: '1.125rem',
+	body: '1rem'
+};

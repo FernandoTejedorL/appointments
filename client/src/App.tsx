ec14970caa5f641ffd2a56f78eeca0acdf4ccd9f
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-05-24 12:03:32 +0200

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
+import AllComp from './components/allComp/AllComp';
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <GlobalStyles />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="/assets/images/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src="/assets/images/react.svg"
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>APPointments</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AllComp />
     </>
   );
 };
---
+++ client/src/components/allComp/AllComp.tsx
@@ -0,0 +1,8 @@
+const AllComp = () => {
+  return (
+    <div>
+      <h1>AllComp</h1>
+    </div>
+  );
+};
+export default AllComp;
